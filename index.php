116b5c39cb079e2a5adb596c8cf62fc79d7208f0
alex1222797 <mauricioalexandergarciacornejo@gmail.com>
2026-06-15 16:00:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
-require "registro.php";
+
+$ruta = $_GET['accion'] ?? '';
+
+switch ($ruta) {
+    case 'registro':
+        require 'registro.php';
+        break;
+
+    case 'update':
+        require 'update.php';
+        break;
+
+    case 'delete':
+        require 'delete.php';
+        break;
+
+    default:
+        echo json_encode([
+            "status" => "error",
+            "mensaje" => "ruta no encontrada"
+        ]);
+}
